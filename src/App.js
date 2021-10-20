f441864194ca3b5e192f36e37bb119aef872b031
AZIZXlaouiti <azizrocke@gmail.com>
2021-10-20 17:05:25 -0400

    add canvas and actioncable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect  , useRef} from 'react';
+import Cable from 'actioncable';
+import CanvasDraw from 'react-canvas-draw';
+const App = () => {
+  const [currentChatMessage, setCurrentChatMessage] = useState('')
+  const [connection, setConnection ] = useState(false);
+  const [chats, setChats] = useState({});
+  const [chatLogs, setChatLogs] = useState([])
+  const prev = useRef(null)
+  const handleClick = (e)=>{
+  // console.log(e.target.name)
+  if (e.target.name === 'clear'){
+    prev.current.clear()
+  }else if(e.target.name === 'undo') {
+    prev.current.undo()
+  }
 
-function App() {
+} 
+  const handleChange =(e)=>{
+     console.log(prev.current)
+  }
+  const loadChats = () => {
+    fetch('http://localhost:3001/chat_messages')
+      .then(resp => resp.json())
+      .then(data => setChatLogs(data))
+  }
+
+  useEffect(() => {
+    if(!connection) {
+      createSocket();
+      loadChats();
+    }
+  }, [connection])
+
+  const updateCurrentChatMessage = (event) => {
+    setCurrentChatMessage(event.target.value);
+  }
+
+  const handleSendEvent = (event) => {
+    event.preventDefault();
+    chats.create( currentChatMessage );
+    setCurrentChatMessage('');
+  }
+
+  const createSocket = () => {
+    let cable = Cable.createConsumer('ws://localhost:3001/cable');
+    const chatConnection = cable.subscriptions.create({
+      channel: 'ChatChannel'
+    }, {
+      connected: () => {},
+      received: async (data) => {
+        const resp = await JSON.parse(data);
+        setChatLogs(resp.chat_messages)
+        console.log(data,'recieved')
+        // setChatLogs(chatLogCopy);
+      },
+      create: (chatContent) => {
+        chatConnection.perform('create', {
+          content: chatContent
+        });
+      }
+    });
+
+    setChats(chatConnection)
+    setConnection(true);
+  }
+  useEffect(()=>{
+    console.log(prev.current,'canvas')
+  })
+  const chatLogLis = chatLogs.map((chat) => {
+    return (
+      <li key={ chat.id }>
+        user says: { chat.content }
+      </li>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='stage'>
+        <h1>Chat</h1>
+        <div className='chat-logs'>
+          <ul>
+          { chatLogLis }
+          </ul>
+        </div>
+        <input
+          type='text'
+          placeholder='Enter your message...'
+          className='chat-input'
+          value={ currentChatMessage }
+          onChange={ updateCurrentChatMessage }
+        />
+        <button
+          className='send'
+          onClick={ handleSendEvent }
         >
-          Learn React
-        </a>
-      </header>
+          Send
+        </button>
+      </div>
+      <div>
+      <button onClick={handleClick} name='clear'>clear</button>
+      <button onClick={handleClick} name='undo'>undo</button>
+    <CanvasDraw brushRadius={5} lazyRadius={7}  canvasWidth={800} 
+    canvasHeight={600} loadTimeOffset={8}  hideInterface={true} ref={prev} onChange={handleChange}/>
+      
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React ,{ createContext }from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import actionCable from 'actioncable' 
+import { BrowserRouter as Router } from 'react-router-dom';
 
+const CableApp = {}
+CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
+export const ActionCableContext = createContext()
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+
+    <ActionCableContext.Provider value={CableApp.cable}>
+        <App />
+    </ActionCableContext.Provider>
+
+  </Router>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
