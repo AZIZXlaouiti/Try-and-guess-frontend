df4d6fccea0f78856a6168d4a843d6bf4b869695
AZIZXlaouiti <azizrocke@gmail.com>
2021-10-28 12:02:55 -0400

    adding readme again and fixing login styling

--- src/components/sessions/Signup.js
+++ src/components/sessions/Signup.js
@@ -1,52 +1,57 @@
-import * as Yup from 'yup';
-import { useState } from 'react';
+import React , { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 import { useFormik, Form, FormikProvider } from 'formik';
 import eyeFill from '@iconify/icons-eva/eye-fill';
 import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
 import { Typography } from '@mui/material';
 import { Link } from '@mui/material';
-// material
 import { Button } from '@mui/material';
 import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
-// import { LoadingButton } from '@mui/lab';
-
 // ----------------------------------------------------------------------
 
 export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
-
+  const [ready , setReady ] = useState(false)
+  // useEffect(()=>{
+  //   if (!Object.values(formik.initialValues).some((value)=>value === "")){
+  //     setReady(true)
+  //   }
+  //   else {
+  //     setReady(false)
+  //   }
+  // },[])
+  // const formik = useFormik({
+  //     username: '',
+  //     password: ''
+   
+  // });
+  // console.log(formik.initialValues , "formik")
+  // const handleChange = (e)=>{
+  //   console.log(e.target.name)
+  //   formik.initialValues[e.target.name]= e.target.value
+  // }
   
 
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      password: ''
-    }
-  });
-
-  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
-
   return (
-    <FormikProvider value={formik}>
-      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+   
+      <form autoComplete="off" noValidate>
         <Stack spacing={3}>
-         
+         <h1>Signup</h1>
 
           <TextField
             fullWidth
             autoComplete="username"
             type="username"
             label="username"
-         
+            name = 'username'
           />
 
-          <TextField
+<TextField
             fullWidth
             autoComplete="current-password"
             type={showPassword ? 'text' : 'password'}
             label="Password"
-            {...getFieldProps('password')}
+            
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -58,25 +63,28 @@ export default function Signup() {
             }}
         
           />
+        
+          
 
           <Button
             fullWidth
             size="large"
             type="submit"
             variant="contained"
-            loading={isSubmitting}
+            disabled = {!ready}
           >
             Register
           </Button>
             <Typography variant="body2" align="center" sx={{ mt: 3 }}>
              Already have an account?  &nbsp;
-              <Link variant="subtitle2"  to="register">
+              <Link variant="subtitle2"  to="register" underline="hover"   component="button" >
               Login
               </Link>
+              
             </Typography>
         
         </Stack>
-      </Form>
-    </FormikProvider>
+      </form>
+
   );
 }
