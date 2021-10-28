836c4bb2714d5c88a59ced06da9f3419ddcad9a7
AZIZXlaouiti <azizrocke@gmail.com>
2021-10-28 10:03:41 -0400

    small changes

--- src/components/sessions/Signup.js
+++ src/components/sessions/Signup.js
@@ -1,10 +1,11 @@
 import * as Yup from 'yup';
 import { useState } from 'react';
-// import { Icon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import { useFormik, Form, FormikProvider } from 'formik';
-// import eyeFill from '@iconify/icons-eva/eye-fill';
-// import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
-import { useNavigate } from "@reach/router"
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { Typography } from '@mui/material';
+import { Link } from '@mui/material';
 // material
 import { Button } from '@mui/material';
 import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
@@ -12,25 +13,14 @@ import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-export default function RegisterForm() {
-  // const navigate = useNavigate();
+export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
 
-  const RegisterSchema = Yup.object().shape({
-    firstName: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('First name required'),
-    lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Last name required'),
-    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-    password: Yup.string().required('Password is required')
-  });
+  
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
+      username: '',
       password: ''
     }
   });
@@ -41,32 +31,14 @@ export default function RegisterForm() {
     <FormikProvider value={formik}>
       <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
         <Stack spacing={3}>
-          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-            <TextField
-              fullWidth
-              label="First name"
-              {...getFieldProps('firstName')}
-              error={Boolean(touched.firstName && errors.firstName)}
-              helperText={touched.firstName && errors.firstName}
-            />
-
-            <TextField
-              fullWidth
-              label="Last name"
-              {...getFieldProps('lastName')}
-              error={Boolean(touched.lastName && errors.lastName)}
-              helperText={touched.lastName && errors.lastName}
-            />
-          </Stack>
+         
 
           <TextField
             fullWidth
             autoComplete="username"
-            type="email"
-            label="Email address"
-            {...getFieldProps('email')}
-            error={Boolean(touched.email && errors.email)}
-            helperText={touched.email && errors.email}
+            type="username"
+            label="username"
+         
           />
 
           <TextField
@@ -79,13 +51,12 @@ export default function RegisterForm() {
               endAdornment: (
                 <InputAdornment position="end">
                   <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
-                    {/* <Icon icon={showPassword ? eyeFill : eyeOffFill} /> */}
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
                   </IconButton>
                 </InputAdornment>
               )
             }}
-            error={Boolean(touched.password && errors.password)}
-            helperText={touched.password && errors.password}
+        
           />
 
           <Button
@@ -97,6 +68,13 @@ export default function RegisterForm() {
           >
             Register
           </Button>
+            <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+             Already have an account?  &nbsp;
+              <Link variant="subtitle2"  to="register">
+              Login
+              </Link>
+            </Typography>
+        
         </Stack>
       </Form>
     </FormikProvider>
