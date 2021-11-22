75dbef39bb0425827dddf0f1d700f1b47e973932
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-22 17:33:06 -0500

    getting there

--- src/DrawingBoard.tsx
+++ src/DrawingBoard.tsx
@@ -3,7 +3,35 @@ import {
   DrawingBoardContext,
   DrawingBoardContextProps,
 } from './DrawingBoardContext';
+const styles = {
+  canvas: {
+    border: '2px solid #333',
+    margin:'20px 0px'
+  },
 
+  maindiv: {
+    padding: "10px",
+    margin: "auto",
+    width: "800px",
+  },
+
+  button: {
+    border: "0px",
+    margin: "1px",
+    height: "50px",
+    minWidth: "75px",
+  },
+
+  colorSwatches: {
+    red: { backgroundColor: "red" },
+    orange: { backgroundColor: "orange" },
+    yellow: { backgroundColor: "yellow" },
+    green: { backgroundColor: "green" },
+    blue: { backgroundColor: "blue" },
+    purple: { backgroundColor: "purple" },
+    black: { backgroundColor: "black" },
+  },
+};
 interface DrawingBoardProps {
   width: number;
   height: number;
@@ -24,6 +52,7 @@ const DrawingBoard: React.FC<DrawingBoardProps> = (props) => {
     context.setCtx(ctx);
   }, []);
   return (
+    <div style={styles.maindiv}>
     <canvas
       ref={ref}
       width={props.width}
@@ -32,6 +61,20 @@ const DrawingBoard: React.FC<DrawingBoardProps> = (props) => {
       onMouseUp={context.handleMouseUp}
       onMouseMove={context.handleMouseMove}
     ></canvas>
+     <button onClick={ ()=>{
+        const canvas = ref.current as HTMLCanvasElement;
+        canvas.height = props.height;
+        canvas.width = props.width;
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;  ctx.fillStyle = "white";
+        ctx.fillStyle = "grey";
+        ctx.lineWidth = 12;
+        ctx.lineCap = 'round';
+        ctx.clearRect(0, 0, 800, 600);
+        ctx.fillRect(0, 0, 800, 600);}
+     }
+    
+  >CLEAR</button>
+      </div>
   );
 };
 export default DrawingBoard;
