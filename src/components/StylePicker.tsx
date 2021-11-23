e9769a9c4031bd2d564df710861fed607de2a978
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-23 10:20:20 -0500

    small changes

--- src/components/StylePicker.tsx
+++ src/components/StylePicker.tsx
@@ -6,22 +6,24 @@ import {
 const StylePicker: React.FC = () => {
   const context = useContext(DrawingBoardContext) as DrawingBoardContextProps;
   return (
-    <form>
-      <label>Color</label>
-      <input
-        type="color"
-        value={context.color}
-        onChange={context.handleColorChange}
-      />
-      <label> Brush size</label>
-      <input
-        type="range"
-        min={5}
-        max={50}
-        value={context.brushSize}
-        onChange={context.handleBrushSizeChange}
-      />
-    </form>
+    <form id="stylepicker-container">
+    <label htmlFor="color-picker">Brush color</label>
+    <input
+      id="color-picker"
+      type="color"
+      value={context.color}
+      onChange={context.handleColorChange}
+    />
+    <label htmlFor="brush-size"> Brush size</label>
+    <input
+      id="brush-size"
+      type="range"
+      min={5}
+      max={30}
+      value={context.brushSize}
+      onChange={context.handleBrushSizeChange}
+    />
+  </form>
   );
 };
 export default StylePicker;
