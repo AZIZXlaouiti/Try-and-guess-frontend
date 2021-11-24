6774712326f7357c1d3cfc3264be9ae6f78d1947
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-24 00:25:17 -0500

    adding typescript actioncable

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React  , {useEffect} from 'react'
+import { Cable } from 'actioncable'
+import ActionCable from 'actioncable'
 const Chat: React.FC = () => {
+    useEffect(()=>{
+        const cable  = ActionCable.createConsumer("ws://localhost:3001/cable")
+        cable.subscriptions.create('ChatChannel',{
+            connected():void{
+                 console.log('connected')
+            }
+        })
+        console.log('run') 
+    },[])
    return (
        <div id="chatbox-container">
 
-       </div>
+       </div> 
    )
 }
 export default Chat
