aa90b76d4ecbfbb7dbd10ebbcf7d14d64783b73e
AZIZXlaouiti <azizrocke@gmail.com>
2021-11-27 18:33:30 -0500

    praying

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const  express = require('express');
-const  app = express();
-const PORT = process.env.PORT || 3000
-app.use(express.static('build'));
-app.listen(PORT);
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
