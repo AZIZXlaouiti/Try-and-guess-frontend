83c246aad8ecc8ab133d1f8ffdb73f8d6d07cdff
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-08 01:20:13 -0500

    adding account user refrence

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 export const ScoreBoard: React.FC = ()=>{
   const players = useSelector((state:any)=> state.channels)
+  const currentUser = useSelector((state:any)=> state.sessions.user)
     return (
         <div id='scoreboard' >
            {players.map((e,i)=>{
@@ -10,7 +11,7 @@ export const ScoreBoard: React.FC = ()=>{
                
                           <div className='player'>
                           <div className='avatar'><div className='img'></div></div>
-                          <div className='info' key={i}>{e.username}</div>
+                          <div className='info' key={i}><div>{e.username}</div>{currentUser.username === e.username ? <div id='name'>(You)</div>:null}</div>
                           <div>points:{e.score}</div>
                           </div>
              )
