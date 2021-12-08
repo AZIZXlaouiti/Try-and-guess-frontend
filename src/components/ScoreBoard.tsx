9d6136869b7811536e6e723eb0dbe767e7aaeedc
AZIZXlaouiti <azizrocke@gmail.com>
2021-12-08 00:57:24 -0500

    fixing  styling

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -3,8 +3,19 @@ import { useSelector } from 'react-redux'
 export const ScoreBoard: React.FC = ()=>{
   const players = useSelector((state:any)=> state.channels)
     return (
-        <div id='scoreboard'>
-           {players.map((e,i)=><h2 key={i}>{e.username}</h2>)}
+        <div id='scoreboard' >
+           {players.map((e,i)=>{
+
+             return (
+               
+                          <div className='player'>
+                          <div className='avatar'><div className='img'></div></div>
+                          <div className='info' key={i}>{e.username}</div>
+                          </div>
+             )
+                          })
+           }
+       
         </div>
     )
 }
